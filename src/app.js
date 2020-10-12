beff15310b0ea786b7815b67785c28e865299cd0
bonetou <h.boneto@outlook.com>
2020-10-12 16:05:37 -0300

    mongodb conn

--- src/app.js
+++ src/app.js
@@ -2,9 +2,12 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 
+// database connection
+mongoose.connect('mongodb+srv://boneto:boneto@cluster0.rpyi0.azure.mongodb.net/balta?retryWrites=true&w=majority')
 // load routes
 const indexRoute = require('./routes/index-route');
 const productRoute = require('./routes/product-route')
