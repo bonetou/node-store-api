c09368ce78b3f29ecf46f00d691a30b6697a58f4
bonetou <h.boneto@outlook.com>
2020-10-15 18:45:06 -0300

    async-await

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -5,47 +5,45 @@ const Product = mongoose.model('Product');
 const ValidationContract = require('../validators/fluent-validator');
 const repository = require('../repositories/product-repository');
 
-exports.get = (req, res, next) => {
-    repository
-        .get()
-        .then(data => {
-            res.status(200).send(data);
-        }).catch(e => {
-            res.status(400).send(e);
+exports.get = async (req, res, next) => {
+    try {
+        var data = await repository.get();
+        res.status(200).send(data);
+    } catch (e) {
+        res.status(500).send({
+            message: 'request error'
         });
+    }
 }
 
-exports.getBySlug = (req, res, next) => {
-    repository
-        .getBySlug(req.params.slug)
-        .then(data => {
-            res.status(200).send(data);
-        }).catch(e => {
-            res.status(400).send(e);
-        });
+exports.getBySlug = async (req, res, next) => {
+    try {
+        var data = await respository.getBySlug(req.params.slug);
+        res.status(200).send(data);
+    } catch (e) {
+        res.status(400).send(e);
+    }
 }
 
-exports.getById = (req, res, next) => {
-    repository
-        .getById(req.params.id)
-        .then(data => {
-            res.status(200).send(data);
-        }).catch(e => {
-            res.status(400).send(e);
-        });
+exports.getById = async (req, res, next) => {
+    try {
+        var data = await repository.getById(req.params.id);
+        res.status(200).send(data);
+    } catch (e) {
+        res.status(400).send(e);
+    }
 }
 
-exports.getByTag = (req, res, next) => {
-    repository
-        .getByTag(req.params.tag)
-        .then(data => {
-            res.status(200).send(data);
-        }).catch(e => {
-            res.status(400).send(e);
-        });
+exports.getByTag = async (req, res, next) => {
+    try {
+        var data = await repository.getByTag(req.params.tag);
+        return data;
+    } catch (e) {
+        res.status(400).send(e);
+    }
 }
 
-exports.post = (req, res, next) => {
+exports.post = async (req, res, next) => {
     let contract = new ValidationContract();
     contract.hasMinLen(req.body.title, 3, 'title must contain at least 3 characters');
     contract.hasMinLen(req.body.slug, 3, 'slug must contain at least 3 characters');
@@ -56,45 +54,43 @@ exports.post = (req, res, next) => {
         return;
     }
 
-    repository.create(req.body)
-        .then(x => {
-            res.status(201).send({
-                message: 'registered product'
-            });
-        }).catch(e => {
-            res.status(400).send({
-                message: 'registered product failed',
-                data: e
-            });
-
+    try {
+        await repository.create(req.body);
+        res.status(201).send({
+            message: 'registered product'
         });
-};
-
-exports.put = (req, res, next) => {
-    repository.update(req.params.id, req.body)
-        .then(x => {
-            res.status(201).send({
-                message: 'product updated'
-            });
-        }).catch(e => {
-            res.status(400).send({
-                message: 'product update error',
-                data: e
-            });
+    } catch (e) {
+        res.status(400).send({
+            message: 'registered product failed',
+            data: e
         });
-
+    }
 }
 
-exports.delete = (req, res, next) => {
-    repository.delete(req.params.id)
-        .then(x => {
-            res.status(201).send({
-                message: 'product removed'
-            });
-        }).catch(e => {
-            res.status(400).send({
-                message: 'product remove error',
-                data: e
-            });
+exports.put = async (req, res, next) => {
+    try {
+        await repository.update(req.params.id, req.body);
+        res.status(201).send({
+            message: 'product updated'
         });
+    } catch (e) {
+        res.status(400).send({
+            message: 'product update error',
+            data: e
+        });
+    }
+}
+
+exports.delete = async (req, res, next) => {
+    try {
+        await repository.delete(req.params.id);
+        res.status(201).send({
+            message: 'product removed'
+        });
+    } catch (e) {
+        res.status(400).send({
+            message: 'product remove error',
+            data: e
+        });
+    }
 }
